ed2e7b1814165f9e8b92af40bbf9439265a2e58a
Andrejs Semivragovs <andrejs.semivragovs@testdevlab.com>
2021-02-10 13:38:36 +0200

    Latest commit

---
+++ scripts/test_script.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "--- Test script"
+echo "step env var 1: $STEP_ENV_VAR_1"
+echo "step env var 2: $STEP_ENV_VAR_2"
+
+echo "executor var 1: $executor_env_var_1"
+echo "executor var 2: $executor_env_var_2"
+echo "executor var 3: $EXC_ENV_VAR_3"
+echo "executor var 4: $EXC_ENV_VAR_4"
+echo "executor var 5: $EXC_ENV_VAR_5"
+
+echo "test 1"
+if [ $EXC_ENV_VAR_5 -ge 11 ]; then
+echo "yes"
+else
+echo "no"
+fi
+
+echo "test 2"
+if [ $EXC_ENV_VAR_5 -eq 012 ]; then
+echo "yes"
+else
+echo "no"
+fi
+
+echo "test 3"
+if [ $EXC_ENV_VAR_5 = "012" ]; then
+echo "yes"
+else
+echo "no"
+fi
+
+echo "job var 1: $JOB_ENV_VAR_1"
+echo "job var 2: $JOB_ENV_VAR_2"
+echo "job var 3: $JOB_ENV_VAR_3"
+echo "job var 4: $JOB_ENV_VAR_4"
+echo "job var 5: $JOB_ENV_VAR_5"
+echo "job var 6: $JOB_ENV_VAR_6"
+echo "job var 7: $JOB_ENV_VAR_7"
+
+
