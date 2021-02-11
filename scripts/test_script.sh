2646af26e9732ec4ccd522d477940917c2bc5d07
Andrejs Semivragovs <andrejs.semivragovs@testdevlab.com>
2021-02-11 10:39:46 +0200

    Test command exit

--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -4,14 +4,18 @@ echo "--- Test script"
 echo "step env var 1: $STEP_ENV_VAR_1"
 echo "step env var 2: $STEP_ENV_VAR_2"
 
-echo "executor var 1: executor_env_var_1"
-echo "executor var 2: executor_env_var_2"
+echo "executor var 1: $executor_env_var_1"
+echo "executor var 2: $executor_env_var_2"
 echo "executor var 3: $EXC_ENV_VAR_3"
 echo "executor var 4: $EXC_ENV_VAR_4"
 echo "executor var 5: $EXC_ENV_VAR_5"
 
 echo "job var 1: $JOB_ENV_VAR_1"
 echo "job var 2: $JOB_ENV_VAR_2"
-echo "job var 2: $JOB_ENV_VAR_3"
+echo "job var 3: $JOB_ENV_VAR_3"
+echo "job var 4: $JOB_ENV_VAR_4"
+echo "job var 5: $JOB_ENV_VAR_5"
+echo "job var 6: $JOB_ENV_VAR_6"
+echo "job var 7: $JOB_ENV_VAR_7"
 
 
